234990bc3e07a527aa6e26abfd14baf70473a120
Ubaniak <bhavek.budhia@gmail.com>
2013-09-29 00:52:34 -0500

    linked account to change log

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,5 @@ class Account < ActiveRecord::Base
     belongs_to :account_holder
     belongs_to :area
     belongs_to :catagory
+    has_many :change_logs
 end
--- app/models/change_log.rb
+++ app/models/change_log.rb
@@ -1,3 +1,4 @@
 class ChangeLog < ActiveRecord::Base
     belongs_to :admin
+    belongs_to :account
 end
