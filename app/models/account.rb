41298e2cb7c908dd6f6283f406d81aedc46e04fc
Ubaniak <bhavek.budhia@gmail.com>
2013-09-29 00:47:15 -0500

    change log expiriment

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,19 @@ class Account < ActiveRecord::Base
     belongs_to :area
     belongs_to :catagory
     has_many :change_logs
+
+    def log_changes(params)
+        columns = [ :accno,
+       :plotno,
+       :address,
+       :area_id,
+       :catagory_id,
+       :property_value,
+       :hecter,
+       :description,
+       :land_value,
+       :improved,
+       :TRV,
+       :account_holder_id]
+    end
 end
